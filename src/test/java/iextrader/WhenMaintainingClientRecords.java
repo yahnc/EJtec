d53ba745e7586f6f63427e1c8644537ff3b264cc
yahnc <sebastianhpancho@gmail.com>
2022-11-01 12:06:12 -0500

    Delete employee

--- src/test/java/iextrader/WhenMaintainingClientRecords.java
+++ src/test/java/iextrader/WhenMaintainingClientRecords.java
@@ -1,6 +1,6 @@
-package iextrader;
+package employee;
 
-import iextrader.model.Client;
+import employee.model.Client;
 import io.restassured.RestAssured;
 import net.serenitybdd.rest.SerenityRest;
 import org.junit.Before;
@@ -17,44 +17,27 @@ public class WhenMaintainingClientRecords {
 
     @Before
     public void prepareTestData() {
-        RestAssured.baseURI = "http://localhost:8080/api";
+        RestAssured.baseURI = "http://dummy.restapiexample.com/api/v1/employee/1";
 
         // Given an existing client
 
         Client newClient = Client.Builder.aClient()
-                .withFirstName("Sarah-Jane")
-                .withLastName("Smith")
-                .withEmail("sarah-jane@smith.com")
-                .build();
+        .withemployee_name("Sarah-Jane SMith")
+        .withemployee_salary("1200000")
+        .withemployee_age("19")
+        .withprofile_image("")
+        .build();
 
-        given().contentType("application/json").
-                and().body(newClient)
+        given().contentType("application/json")
+                .and().body(newClient)
                 .when().post("/client")
-                .then().statusCode(200);
+                .then().statusCode(200)
+                .and().body("id", not(equalTo(0)));
 
         clientId = SerenityRest.lastResponse().jsonPath().getString("id");
 
     }
-
-    @Test
-    public void should_be_able_to_update_an_existing_client() {
-
-        Client updatedClient = Client.Builder.aClient().withFirstName("Sarah-Jane")
-                .withLastName("Smith")
-                .withEmail("sarah-jane.smith@myemail.com")
-                .build();
-
-        given().contentType("application/json")
-                .and().body(updatedClient)
-                .and().pathParam("id",clientId)
-                .when().put("/client/{id}")
-                .then().statusCode(200);
-
-        given().pathParam("id", clientId)
-                .when().get("/client/{id}")
-                .then().body("email", equalTo("sarah-jane.smith@myemail.com"));
-
-    }
+}
 
     @Test
     public void should_remove_client_from_the_registry() {
